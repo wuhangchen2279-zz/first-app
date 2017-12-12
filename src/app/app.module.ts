9a0581b4ea5a34fbf22600e98097f0877d7bae7d
Daniel Wu <daniel@ofsystems.com>
2017-12-12 15:30:56 +1100

    data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 
@@ -14,7 +15,8 @@ import { ServersComponent } from './servers/servers.component';
     ServersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { setTimeout } from 'timers';
 
 @Component({
     //selector: 'app-servers',
     //selector: '[app-servers]',
     selector: '.app-servers',
-    template: `<app-server></app-server><app-server></app-server>`,
+    templateUrl: './servers.component.html'
+    //template: `<app-server></app-server><app-server></app-server>`,
     //styleUrls: ['./'],
 })
 export class ServersComponent {
+    allowNewServer = false;
+    serverCreationStatus = 'no server created';
+    serverName = 'TestServer';
 
+    constructor() {
+        setTimeout(()=> {
+            this.allowNewServer = true;
+        },2000)
+    }
+
+    onUpdateServerName(event: any) {
+        //onsole.log(event);
+        this.serverName = (<HTMLInputElement>event.target).value;
+    }
+
+    onCreateServer() {
+        this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+    }
 }
