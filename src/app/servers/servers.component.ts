cd5fa18918d5d603105c18934ffe77b76724c49e
Daniel Wu <daniel@ofsystems.com>
2017-12-12 11:27:19 +1100

    angular server components commit

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-servers',
-    template: '<app-server></app-server><app-server></app-server>'
+    //selector: 'app-servers',
+    //selector: '[app-servers]',
+    selector: '.app-servers',
+    template: `<app-server></app-server><app-server></app-server>`,
+    //styleUrls: ['./'],
 })
 export class ServersComponent {
 
